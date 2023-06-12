e240999de3fda3f4c8b6cdb68f820db2024bd554
Victor Mihaita <43206741+victormihaita@users.noreply.github.com>
2023-06-12 10:30:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,6 @@
 import PackageDescription
 
 let package = Package(name: "ApphudSDK",
-                        platforms: [.iOS(.v13), .macOS(.v10_11)],
+                        platforms: [.iOS(.v13), .macOS(.v11)],
                         products: [.library(name: "ApphudSDK", targets: ["ApphudSDK"])],
                         targets: [.target(name: "ApphudSDK", path: "ApphudSDK")])
